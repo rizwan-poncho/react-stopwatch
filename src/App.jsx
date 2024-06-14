b01c4eb39a68ca689fc2173d5bf736cb715debb3
rizwan-poncho <rizwan.sp21@gmail.com>
2024-06-14 15:11:38 +0600

    stopwatch completed and added tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [time, setTime] = useState(0);
+
+  const [running, setRunning] = useState(false);
+
+  useEffect(() => {
+    let interval;
+
+    if (running) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
+    } else if (!running && time !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }),
+    [running];
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <section>
+        <h1 className="text-center font-bold text-4xl py-10">STOPWATCH</h1>
+        <div className="flex justify-center py-10">
+          <span className="text-6xl">
+            {("0" + Math.floor((time / 60000) % 60)).slice(-2)}:
+          </span>
+          <span className="text-6xl">
+            {("0" + Math.floor((time / 1000) % 60)).slice(-2)}:
+          </span>
+          <span className="text-6xl">
+            {"0" + Math.floor((time / 10) % 100)}
+          </span>
+        </div>
+        <div className="flex justify-center gap-5">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={() => setRunning(true)}
+          >
+            Start
+          </button>
+          <button
+            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+            onClick={() => setRunning(false)}
+          >
+            Stop
+          </button>
+          <button
+            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+            onClick={() => setTime(0)}
+          >
+            Reset
+          </button>
+        </div>
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
